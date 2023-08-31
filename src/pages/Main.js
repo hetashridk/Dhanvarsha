cf80a4ed7752edf709b7673f16dfef1682b7a824
hetashridk <hetashrik@gmail.com>
2023-08-31 23:15:59 +0530

    src folder added

---
+++ src/pages/Main.js
@@ -0,0 +1,12 @@
+import Navbar from "../../components/Navbar";
+
+function Main() {
+    return (
+        <>
+            {/* <h1>Home</h1> */}
+            <Navbar />
+        </>
+    );
+}
+
+export default Main;
